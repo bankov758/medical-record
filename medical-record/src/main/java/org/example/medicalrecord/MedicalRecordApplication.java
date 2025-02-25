60b8c5fcea24d08b4e40410fce399e8fcf7eda19
Ivo Bankov <ivobankov758@gmail.com>
2025-02-25 23:31:37 +0200

    make it better31

--- medical-record/src/main/java/org/example/medicalrecord/MedicalRecordApplication.java
+++ medical-record/src/main/java/org/example/medicalrecord/MedicalRecordApplication.java
@@ -20,6 +20,7 @@ public class MedicalRecordApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MedicalRecordApplication.class, args);
+        //dasdasdad
     }
 
     @Bean
